047accabdf74c887708bf1f667d9a98160d73b3c
tornike <tormotsadze@unisens.ge>
2022-03-30 15:29:51 +0400

    Upload1

---
+++ Task1/weight.rb
@@ -0,0 +1,22 @@
+#ideal weight
+
+class Ideal
+  MARGIN = 110
+  def initialize
+    puts "Enter your name: "
+    @name = gets.chomp.capitalize
+    puts "Enter your height: "
+    @height = gets.chomp.to_i
+  end
+
+  def weight
+    difference = @height - MARGIN
+    if difference.negative?
+      "Your weight is already optimal"
+    else
+      "#{@name} your ideal weight is #{difference}"
+    end
+  end
+end
+
+puts Ideal.new.weight
