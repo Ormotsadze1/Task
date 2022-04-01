d1e90c2cef0548d0891be371ea712ae696be63e9
Ormotsadze1 <tormotsadze@unisens.ge>
2022-04-01 11:02:15 +0400

    add new line

--- number/numbers_from.rb
+++ number/numbers_from.rb
@@ -1,10 +1,11 @@
 #Fill in the array with numbers from 10 to 100 in increments of 5
 
+MINIMUM = 10
+MAXIMUM = 100
+FIVE = 5
+ZERO = 0
+
 class Numbers
-  MINIMUM = 10
-  MAXIMUM = 100
-  FIVE = 5
-  ZERO = 0
   def five_increment
     arr = [ *MINIMUM..MAXIMUM].select { |number| number % FIVE == ZERO }
     arr
