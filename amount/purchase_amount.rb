7339074d00bfaa05ca14309cfef7d75a931e20e5
Ormotsadze1 <tormotsadze@unisens.ge>
2022-04-01 10:49:26 +0400

    add new line

--- amount/purchase_amount.rb
+++ amount/purchase_amount.rb
@@ -1,37 +1,43 @@
-class Shop
+class Shop 
   def initialize
     @list = {}
     @price_quantitiy = []
   end
+
   def user_input
     loop do
-      puts "Please enter item:"
-      @name = gets.chomp()
-      break if @name.downcase == "stop"
-      puts "Please eneter price"
-      @price = gets.chomp()
-      puts "Please enter quantity:"
-      @quantity = gets.chomp()
-      @list[@name] = [@price.to_i,@quantity.to_i]
+    puts "Please enter item:"
+    @name = gets.chomp()
+    break if @name.downcase == "stop"
+    puts "Please eneter price"
+    @price = gets.chomp()
+    puts "Please enter quantity:"
+    @quantity = gets.chomp()
+    @list[@name] = [@price.to_i,@quantity.to_i]
+    @price_quantitiy = [@price.to_i,@quantity.to_i]
     end
-    @list
+    @list 
   end
+
   def plain_list
     user_input
   end
+
   def one_item_at_a_time
-    @list.each{|k,v| @list[k]=v.inject(:*)}
+    @list.each{|name,_| @list[name] = @price_quantitiy.inject(:*)}
   end
+
   def total_sum
     one_item_at_a_time.values.inject(:+)
   end
-  def newone
-    plain_list
-    one_item_at_a_time
-    total_sum
+
+  def order_details
+    puts plain_list
+    puts one_item_at_a_time
+    puts "your total is #{total_sum}"
   end
 end
 
 
 
-Shop.new.newone
+Shop.new.order_details
