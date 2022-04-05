f0d0d3335792e2da65a67f13d73ba5a19aaab775
Ormotsadze1 <tormotsadze@unisens.ge>
2022-04-05 09:46:56 +0400

    add new names

--- Triangle/area.rb
+++ Triangle/area.rb
@@ -1,16 +1,42 @@
-#The area of a triangle.
-NUMBER = 2
-class Area
+# frozen_string_literal: true
+
+# The area of a triangle.
+class CalculateTriangleAreaService
+  DIVIDER = 2
+  EDGE = 0
+
   def initialize
-    puts "What is the base of a triangle?"
-    @base = gets.chomp.to_i
-    puts "What is the height of a triangle?"
-    @height = gets.chomp.to_i
+    puts 'What is the base of a triangle?'
+    @base = gets.chomp
+    puts 'What is the height of a triangle?'
+    @height = gets.chomp
   end
 
-  def triangle
-    (@base * @height) / NUMBER
+  def call
+    validate_base_parameter
+    validate_height_parameter
+    calculate_area
+  end
+
+  private
+
+  def validate_base_parameter
+    return unless @base.to_i <= EDGE || @base.count('a-zA-Z').positive?
+
+    warn 'wrong input for base'
+    exit 1
+  end
+
+  def validate_height_parameter
+    return unless @height.to_i <= EDGE || @height.count('a-zA-Z').positive?
+
+    warn 'wrong input for height'
+    exit 1
+  end
+
+  def calculate_area
+    (@base.to_i * @height.to_i) / DIVIDER
   end
 end
 
-puts Area.new.triangle
+puts CalculateTriangleAreaService.new.call
