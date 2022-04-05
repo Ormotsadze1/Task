89e8403788a605477ff31ac3b395cd11a41c1b63
Ormotsadze1 <tormotsadze@unisens.ge>
2022-04-05 09:34:40 +0400

    add new line

--- Task1/weight.rb
+++ Task1/weight.rb
@@ -1,21 +1,38 @@
-#ideal weight
-MARGIN = 110
-class Ideal
+# frozen_string_literal: true
+
+# display the ideal weight using the formula <height> - 110
+class IdealWeight
+  SUBSTRACTOR = 110
+
   def initialize
-    puts "Enter your name: "
+    puts 'Enter your name: '
     @name = gets.chomp.capitalize
-    puts "Enter your height: "
-    @height = gets.chomp.to_i
+    puts 'Enter your height: '
+    @height = gets.chomp
   end
 
-  def weight
-    difference = @height - MARGIN
+  def call
+    validate_parameters
+    detect_ideal_weight
+  end
+
+  private
+
+  def validate_parameters
+    return unless @height.to_i.negative? || @height.count('a-zA-Z').positive?
+
+    warn 'incorrect input for height!'
+    exit 1
+  end
+
+  def detect_ideal_weight
+    difference = @height.to_i - SUBSTRACTOR
     if difference.negative?
-      "Your weight is already optimal"
+      "#{@name}, your weight is already optimal"
     else
-      "#{@name} your ideal weight is #{difference}"
+      "#{@name}, your ideal weight is #{difference}"
     end
   end
 end
 
-puts Ideal.new.weight
+puts IdealWeight.new.call
