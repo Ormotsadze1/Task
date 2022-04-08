0b103e55dcc1d1920d2c30f7af11a513d48c823a
Ormotsadze1 <tormotsadze@unisens.ge>
2022-04-08 10:45:44 +0400

    numbersarea

---
+++ week4/primenumbersare.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+require 'prime'
+
+# Display sum of Prime Numbers
+class CauntPrimeService
+  ALPHABETICS = /\D/.freeze
+
+  def initialize
+    @numbers = []
+    @primes = []
+  end
+
+  def call
+    take_numbers
+    validate_parameters
+    find_primes
+    sum_of_primes
+  end
+
+  private
+
+  def take_numbers
+    input = ''
+    while input
+      puts 'Input numbers (enter "stop" to finish)'
+      input = gets.chomp
+      break if input == 'stop'
+
+      @numbers << input
+    end
+    @numbers
+  end
+
+  def validate_parameters
+    return if @numbers.join('').scan(ALPHABETICS).empty?
+
+    warn 'Input must include only numbers'
+    exit 1
+  end
+
+  def find_primes
+    @primes = @numbers.map(&:to_i).select { |num| Prime.prime?(num) }
+  end
+
+  def sum_of_primes
+    @primes.inject(0, :+)
+  end
+end
+
+puts CauntPrimeService.new.call
+
